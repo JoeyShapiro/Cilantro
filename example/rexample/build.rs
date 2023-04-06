b19ff1a6e522a067e05056eacaaca4559c6d12b7
JoeyShapiro <joeyashapiro@gmail.com>
2023-04-06 15:29:47 -0500

    rust uses static, i think

--- example/rexample/build.rs
+++ example/rexample/build.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("cargo:rustc-link-lib=Cilantro");
     // and it should search the Cargo.toml directory for that library
     println!("cargo:rustc-link-search=.");
+    println!("cargo:rustc-link-lib=c++"); // gcc needs stdc++, but rust needs c++ (mac uses libc++.a)
+    // not sure, it seems like static is a little bigger (455K -> 462K)
 }
